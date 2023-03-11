e99c563d721f522846f8df79e985cc54bcbe4715
Kurucz János <kuruczjanoshivatalos@gmail.com>
2023-03-11 14:09:00 +0100

    Idézet és névjegy hozzáadva

--- src/app.js
+++ src/app.js
@@ -143,3 +143,23 @@ function clearFields() {
     useInput.value = "";
     priceInput.value = "";
 }
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+const alert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    alert('"Tedd, vagy ne tedd, de ne próbáld!" \n Star Wars -- Yoda', 'success')
+  })
+}
